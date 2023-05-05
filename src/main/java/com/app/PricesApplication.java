053bc4b4bb231f4df595abd5c5a7535c393e178e
CarlosTheCrabbo <carloscinicshady@gmail.com>
2023-05-05 13:16:42 +0200

    error: constructor in PriceUseCases required a bean that already has

--- src/main/java/com/app/PricesApplication.java
+++ src/main/java/com/app/PricesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+
 public class PricesApplication {
     public static void main(String[] args) {
         SpringApplication.run(PricesApplication.class, args);
